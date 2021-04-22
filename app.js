1970d30736ec3429d19c810e99c6f5c2682b412a
Binh Do <binhdq@fe.edu.vn>
2021-04-22 16:49:38 +0700

    user functions

--- app.js
+++ app.js
@@ -18,6 +18,38 @@ app.get('/',(req,res)=>{
         })
     }   
 )
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }))
+app.post('/doLogin',(req,res)=>{
+    var role = req.body.role;
+    var adminValue ='';
+    if(role=='admin')
+        adminValue = true;
+    
+    res.render('home',{
+        pageTitle : 'Home page',
+        admin: adminValue
+    })
+    
+})
+
+hbs.registerHelper('taoCombo',(city)=>{
+    var cities = city.split(';')
+    var combo = '<select>';
+    for(i=0;i<cities.length;i++){
+        combo = combo.concat('<option>'+ cities[i] + '</option>')
+    }
+    combo = combo.concat('</select>');
+    console.log(combo);
+    return combo;
+})
+
+hbs.registerHelper('toUpper',(msg)=>{
+    return msg.toUpperCase();
+})
 
 app.listen(5000)
 console.log('Server is running!')
